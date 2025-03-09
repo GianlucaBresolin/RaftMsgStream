b793d02028e096b05d7201bdba41138202c822da
Gianluca Bresolin <gianbreso02@gmail.com>
2025-03-09 16:06:15 +0100

    Fix: NewServer.

--- launchers/server/main.go
+++ launchers/server/main.go
@@ -11,8 +11,8 @@ func main() {
 	log.Println("Starting server in unvoting mode", os.Args[1])
 	server := server.NewServer(
 		raft.ServerID("node1"),
-		raft.Port(":5001"),
-		map[raft.ServerID]raft.Port{},
+		raft.Address("localhost:5001"),
+		map[raft.ServerID]raft.Address{},
 		os.Args[1] == "true",
 	)
 
--- server/server.go
+++ server/server.go
@@ -13,10 +13,10 @@ type Server struct {
 	stateMachine *msgStreamStateMachine
 }
 
-func NewServer(id raft.ServerID, port raft.Port, peers map[raft.ServerID]raft.Port, unvoting bool) *Server {
+func NewServer(id raft.ServerID, address raft.Address, peers map[raft.ServerID]raft.Address, unvoting bool) *Server {
 	server := rpc.NewServer()
 
-	raftNode := raft.NewRaftNode(id, port, server, peers, unvoting)
+	raftNode := raft.NewRaftNode(id, address, server, peers, unvoting)
 	s := &Server{
 		raftNode:     raftNode,
 		stateMachine: newMsgStreamStateMachine(string(id), raftNode.CommitCh, raftNode.SnapshotRequestCh, raftNode.SnapshotResponseCh, raftNode.ApplySnapshotCh, raftNode.ReadStateCh, raftNode.ReadStateResultCh),
@@ -31,11 +31,11 @@ func NewServer(id raft.ServerID, port raft.Port, peers map[raft.ServerID]raft.Po
 	mux := http.NewServeMux()
 	mux.Handle(rpc.DefaultRPCPath, server)
 
-	listener, err := net.Listen("tcp", string(port))
+	listener, err := net.Listen("tcp", string(address))
 	if err != nil {
-		log.Fatalf("Failed to listen on port %s: %v", string(port), err)
+		log.Fatalf("Failed to listen on address %s: %v", string(address), err)
 	}
-	log.Printf("Server %s is listening on %s\n", string(id), string(port))
+	log.Printf("Server %s is listening on %s\n", string(id), string(address))
 
 	go http.Serve(listener, mux)
 
